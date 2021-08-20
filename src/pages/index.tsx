1c08e716940039e2b8f47f0213d5df940d7535de
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-08-20 16:22:18 -0300

    Posts are now functional and comming from the Prismic API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
@@ -27,57 +31,85 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
 
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Algum titulo</strong>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <ul>
-                <li>
-                  <FiCalendar/>
-                  15 Mar 2021
-                </li> 
-                <li>
-                  <FiUser/>
-                  15 Mar 2021
-                </li> 
-              </ul>
-            </a>    
-          </Link>
-          <Link href="/">
-            <a>
-              <strong>Algum titulo</strong>
-              <p>Pensando em sincronização em vez de ciclos de vida.</p>
-              <ul>
-                <li>
-                  <FiCalendar/>
-                  15 Mar 2021
-                </li> 
-                <li>
-                  <FiUser/>
-                  15 Mar 2021
-                </li> 
-              </ul>
-            </a>    
-          </Link>
-          <button type="button" className="button">Carregar mais posts</button>
-      
-        </div>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
 
+          <button type="button" className="button">
+            Carregar mais posts
+          </button>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
