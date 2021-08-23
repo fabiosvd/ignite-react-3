0c0281899b361528861efd2dca9d50d8cdeeb4f3
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-08-23 19:22:08 -0300

    Made post front-end

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,8 +2,12 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 
+import Header from '../../components/Header';
+
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
 
 interface Post {
   first_publication_date: string | null;
@@ -26,9 +30,46 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <img src="/bannerTest.png" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Criando um app CRA do zero</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                23 Aug 2021
+              </li>
+              <li>
+                <FiUser/>
+                Fabio Salvador
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Proin et varius</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              eget ligula eu <strong>lectus lobortis</strong> condimentum. Aliquam nonummy auctor
+              massa. Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac <a href="#">turpis egestas</a>. Nulla at risus. Quisque purus
+              magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis,
+              felis ut adipiscing.
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
